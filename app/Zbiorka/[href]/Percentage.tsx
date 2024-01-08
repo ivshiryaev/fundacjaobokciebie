5424fd03a40162b8c1ff50fa8c9e26ba524e0931
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2024-01-08 11:02:26 +0100

    added background-white to percentage

--- app/Zbiorka/[href]/Percentage.tsx
+++ app/Zbiorka/[href]/Percentage.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 
 function Percentage({percentage}:{percentage: number}) {
 	return (
-		<div className='relative w-full h-[1.75rem] flex justify-center items-center rounded-[2rem] shadow-xl overflow-hidden'>
+		<div className='relative w-full h-[1.75rem] flex justify-center items-center rounded-[2rem] shadow-xl overflow-hidden bg-white'>
 			<motion.div 
 				className={`absolute rounded-full left-0 top-0 h-full ${percentage >= 100 ? 'bg-success' : 'bg-primary'}`}
 				initial={{
